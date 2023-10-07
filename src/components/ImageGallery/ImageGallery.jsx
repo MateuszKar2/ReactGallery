66265cfe08071414e48b6dab2c99f791d4dcb600
MateuszKar2 <system505@protonmail.com>
2023-10-07 19:14:49 +0200

    add file

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,42 +1,16 @@
-import { Component } from 'react';
-import { PropTypes } from 'prop-types';
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
-import { nanoid } from 'nanoid';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 
-export class ImageGallery extends Component {
-  onClickHandle = i => {
-    return e => {
-      e.preventDefault();
-      this.props.stateUpdate('modalIndex', i);
-    };
-  };
-  render() {
-    return (
-      <ul className={css.ImageGallery}>
-        {this.props.gallery.map((img, i) => (
-          <ImageGalleryItem
-            key={nanoid()}
-            id={img.id}
-            miniature={img.miniature}
-            url={img.url}
-            alt={img.tags}
-            onClick={this.onClickHandle(i)}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+export const ImageGallery = ({ images, toggleModal }) => {
+  return (
+    <ul className={css.gallery}>
+      <ImageGalleryItem toggleModal={toggleModal} images={images} />
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
-  gallery: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      url: PropTypes.string,
-      miniature: PropTypes.string,
-      alt: PropTypes.string,
-    })
-  ).isRequired,
-  stateUpdate: PropTypes.func.isRequired,
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  toggleModal: PropTypes.func.isRequired,
 };
