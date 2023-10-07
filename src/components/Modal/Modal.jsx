8431aa4f8d0a7e537890b6d6715e7b8b99a23b36
MateuszKar2 <system505@protonmail.com>
2023-10-07 19:15:11 +0200

    add file

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,32 @@
-import {Component} from "react"
-import css from "./Modal.module.css"
-import { PropTypes } from 'prop-types';
+import React, { useEffect } from 'react';
+import css from './Modal.module.css';
 
-export class Modal extends Component {
-     static defaultProps = {
-        url: 'no url',
-        alt: 'no alt',
-     }
-     handleKeyUp = e => {
-        if (e.key === 'Escape') this.props.stateUpdate('modalIndex', '-1');
-      };
-      closeModal = e => {
-        if (e.target.className === css.Overlay)
-          this.props.stateUpdate('modalIndex', '-1');
-      };
-    
-      componentDidMount() {
-        document.addEventListener('keyup', this.handleKeyUp);
+export const Modal = ({ onCloseModal, largeImageURL, alt }) => {
+  useEffect(() => {
+    const keyDown = evt => {
+      if (evt.code === 'Escape') {
+        onCloseModal();
       }
-    
-      componentWillUnmount() {
-        document.removeEventListener('keyup', this.handleKeyUp);
-      }
-    
-      render() {
-        return (
-          <div className={css.Overlay} onClick={this.closeModal}>
-            <div className={css.Modal}>
-              <img className={css.img} src={this.props.url} alt={this.props.alt} />
-            </div>
-          </div>
-        );
-      }
-    }
-    
-    Modal.propTypes = {
-      url: PropTypes.string,
-      alt: PropTypes.string,
     };
+
+    window.addEventListener('keydown', keyDown);
+
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [onCloseModal]);
+
+  const handleClose = evt => {
+    if (evt.currentTarget === evt.target) {
+      onCloseModal();
+    }
+  };
+
+  return (
+    <div onClick={handleClose} className={css.Overlay}>
+      <div className={css.Modal}>
+        <img src={largeImageURL} alt={alt} />
+      </div>
+    </div>
+  );
+};
